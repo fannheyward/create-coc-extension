97cb2858ee8e0314f3dfe496ab2263e51855974b
Heyward Fann <fannheyward@gmail.com>
2019-08-05 09:44:55 +0800

    feat(template): command/config/sources template

--- template/src/index.ts
+++ template/src/index.ts
@@ -1,5 +1,35 @@
-import { ExtensionContext, workspace } from 'coc.nvim';
+import { commands, CompleteResult, ExtensionContext, sources, workspace } from 'coc.nvim';
 
 export async function activate(context: ExtensionContext): Promise<void> {
   workspace.showMessage(`[title] is works!`);
+
+  context.subscriptions.push(
+    commands.registerCommand('[title].Command', async () => {
+      workspace.showMessage(`[title] Commands works!`);
+    }),
+
+    sources.createSource({
+      name: '[title] completion source', // unique id
+      shortcut: '[CS]', // [CS] is custom source
+      priority: 1,
+      triggerPatterns: [], // RegExp pattern
+      doComplete: async () => {
+        const items = await getItems();
+        return items;
+      }
+    })
+  );
+}
+
+async function getItems(): Promise<CompleteResult> {
+  return {
+    items: [
+      {
+        word: 'TestCompletionItem 1'
+      },
+      {
+        word: 'TestCompletionItem 2'
+      }
+    ]
+  };
 }
